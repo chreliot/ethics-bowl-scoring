04e767dfde4a61a2f3e4f8b4d02863ee8c5be731
Christopher Eliot <eliot001@umn.edu>
2015-12-07 22:59:31 -0500

    Create R script file

---
+++ EBScoring.R
@@ -0,0 +1,5 @@
+# An R script for scoring the Long Island Regional High School Ethics Bowl
+# (c) 2015 Christopher H. Eliot (@chreliot)
+#  This code is annotated, but see the README at https://github.com/chreliot/ethics-bowl-scoring
+
+
